08cb9a68637e11fa0206fe9d26c6cb6209431f07
19UV <The19UV@gmail.com>
2022-06-03 12:45:52 +0000

    Fixed path problem

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
-const in_production = process.env.NODE_ENV == "production";
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: in_production === "production" ? "/snails-till-summer/" : "/",
-  productionSourceMap: !in_production,
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/snails-till-summer/" : "/",
 });
